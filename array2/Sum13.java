ad32dedeccbed13b3338f1f5898e4061c9ea288e
Your Name <you@example.com>
2025-07-29 12:59:22 +0400

    Add solutions for array2-all

---
+++ array2/Sum13.java
@@ -0,0 +1,15 @@
+public int sum13(int[] nums) {
+    int sum = 0;
+    int i = 0;
+      
+    while(i < nums.length) {
+        if(nums[i] == 13) {
+            i += 2;
+        } else {
+            sum += nums[i];
+            i++;
+        }
+    }
+                                          
+    return sum;
+}
