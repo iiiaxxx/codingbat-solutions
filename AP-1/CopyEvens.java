b3511acd00cb4934dbe474814f5d2d6786d3f5d6
Your Name <you@example.com>
2025-08-02 22:01:34 +0400

    Add all AP-1 solutions

---
+++ AP-1/CopyEvens.java
@@ -0,0 +1,13 @@
+public int[] copyEvens(int[] nums, int count) {
+  int[] arr = new int[count];
+    int index = 0;
+      
+    for(int i = 0; index < count; i++) {
+        if(nums[i] % 2 == 0) {
+            arr[index] = nums[i];
+            index++;
+        }
+    }
+                                
+    return arr;
+}
---
+++ AP-1/WordsFront.java
@@ -0,0 +1,8 @@
+public String[] wordsFront(String[] words, int n) {
+   String[] arr = new String[n];
+    
+    for(int i = 0; i < arr.length; i++)
+        arr[i] = words[i];
+              
+    return arr;
+}
