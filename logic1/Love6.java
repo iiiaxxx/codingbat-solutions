164879c8ea3b45187945a59d640586d0c792e702
Your Name <you@example.com>
2025-08-02 21:35:16 +0400

    Add all Logic-1 solutions

---
+++ logic1/Love6.java
@@ -0,0 +1,9 @@
+public boolean love6(int a, int b) {
+  int sum = a + b;
+  int diff = Math.abs(a - b);
+  if (a == 6 || b == 6 || sum == 6 || diff == 6) {
+    return true;
+  }
+  return false;
+  
+}
---
+++ logic1/SumLimit.java
@@ -0,0 +1,10 @@
+public int sumLimit(int a, int b) {
+   int sum = a + b;
+    int aLength = String.valueOf(a).length();
+    int sumLength = String.valueOf(sum).length();
+        
+    if(sumLength == aLength)
+        return sum;
+                  
+    return a;
+}
